3f9fff01d161276e36ba125de998b6ecc9b007af
EphantusG <ephantus.mwihaki@gmail.com>
2023-02-16 10:48:31 -0800

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * mai-A function that prints out a statement using the puts function
+ * ret: 0 --Execution success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n",stdou);
+		return (0);
+}
