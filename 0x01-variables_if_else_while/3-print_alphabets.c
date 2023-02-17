3c4062d6f5e8f2c0a2459531f1e83887379ca1d8
EphantusG <ephantus.mwihaki@gmail.com>
2023-02-17 13:06:19 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main- Start of double while loops
+ *Return: 0 (For success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	char A = 'A';
+
+	a = 'a';
+	A = 'A';
+	while
+		(a <= 'z') {
+			putchar(a);
+			a++;
+		}
+	while
+		(A <= 'Z') {
+			putchar(A);
+			A++;
+		}
+	putchar('\n');
+	return (0);
+}
