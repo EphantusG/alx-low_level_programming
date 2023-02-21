790e88b881a5709e69ac9a89e3f89b16c83d29c1
EphantusG <ephantus.mwihaki@gmail.com>
2023-02-21 12:05:23 -0800

    Update description

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- *print_alphabet_x10: Print out lowercase alphabets 10 times
+ *print_alphabet_x10 - print out lowercase alphabets, 10 times
  *ending in a new line
  */
 void print_alphabet_x10(void)
