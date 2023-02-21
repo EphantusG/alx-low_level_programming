8228b4ddf9e986607d43f0bff843e842142437ba
EphantusG <ephantus.mwihaki@gmail.com>
2023-02-21 11:19:31 -0800

    Prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include"main.h"
-#include"_putchar.c"
 /**
  *main - print _putchar
  *Return: 0 Always success
@@ -7,5 +6,14 @@
 int main(void)
 {
 	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
