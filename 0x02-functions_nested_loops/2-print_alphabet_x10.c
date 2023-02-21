8d72fbaa1193792da92f80ae6b67410d72605961
EphantusG <ephantus.mwihaki@gmail.com>
2023-02-21 12:02:07 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ *print_alphabet_x10: Print out lowercase alphabets 10 times
+ *ending in a new line
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
