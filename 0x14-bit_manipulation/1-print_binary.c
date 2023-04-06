ad80c922b6f30d5b5b2b58c9b24d2647042d3e01
EphantusG <ephantus.mwihaki@gmail.com>
2023-04-06 13:23:12 -0700

    Write a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,4 +3,23 @@
  * print_binary - converts a base 10 to base 2
  * @b : binary number to be printed
  */
+void print_binary(unsigned long int b)
+{
+	int n, loc = 0;
+	unsigned long int position;
 
+	for (n = 63; n >= 0; n--)
+	{
+		position = b >> n;
+
+		if (position & 1)
+		{
+			_putchar('1');
+			loc++;
+		}
+		else if (loc)
+			_putchar('0');
+	}
+		if (!loc)
+			_putchar('0');
+}
